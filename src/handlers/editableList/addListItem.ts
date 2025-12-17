839d7567f26adb6faf94d39833cea3ea7776bb07
Younes Alhyan <gameryounes555@gmail.com>
2025-12-17 23:03:33 +0000

    feat(handler): update addListItem to handle ratings list with rating option

--- src/handlers/editableList/addListItem.ts
+++ src/handlers/editableList/addListItem.ts
@@ -2,7 +2,17 @@ import { operationsMap } from "../../utils/operationsMap.js";
 import { imdbGraphQL } from "../../utils/imdbGraphQl.js";
 import { withVariables } from "../../utils/withVariables.js";
 
-const getOperation = (endpoint: string, listId: string, titleId: string) => {
+const getOperation = (
+  endpoint: string,
+  listId: string,
+  titleId: string,
+  options?: {
+    rating?: number;
+  }
+) => {
+  if (listId === "ratings") {
+    return withVariables(operationsMap.AddRating, titleId, options?.rating);
+  }
   return withVariables(operationsMap.AddItemToList, listId, titleId);
 };
 
@@ -10,14 +20,17 @@ export const addListItem = async (
   endpoint: string,
   listId: string,
   titleId: string,
-  getCookie: () => string
+  getCookie: () => string,
+  options?: {
+    rating?: number;
+  }
 ): Promise<boolean> => {
-  const op = getOperation(endpoint, listId, titleId);
+  const op = getOperation(endpoint, listId, titleId, options);
 
   const cookie = getCookie();
   try {
     const response = await imdbGraphQL(cookie, op);
-    
+
     if (response.data) return true;
     return false;
   } catch (error) {

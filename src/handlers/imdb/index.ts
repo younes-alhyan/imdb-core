e4e3bd78576bc811f98a4e1baffb6dc41027e622
Younes Alhyan <gameryounes555@gmail.com>
2025-12-20 01:01:08 +0000

    feat(handlers): add imdb searchTitle handler module

--- src/handlers/imdb/index.ts
+++ src/handlers/imdb/index.ts
@@ -2,10 +2,12 @@ import { setSessionHandler } from "../../handlers/imdb/setSessionHandler.js";
 import { getPredefinedListsHandler } from "./getPredefinedListsHandler.js";
 import { getUserListsHandler } from "./getUserListsHandler.js";
 import { getPublicListHandler } from "./getPublicListHandler.js";
+import { searchHandler } from "./searchHandler.js";
 
 export {
   setSessionHandler,
   getPredefinedListsHandler,
   getUserListsHandler,
   getPublicListHandler,
+  searchHandler
 };

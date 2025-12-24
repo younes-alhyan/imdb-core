ead8545c24a22e5f22cc21b72711b7b8f6a572f3
Younes Alhyan <gameryounes555@gmail.com>
2025-12-24 10:05:06 +0000

    refactor: restructure exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,42 @@
-export { ImdbClient, ImmutableListClient, EditableListClient } from "./client/index.js";
-export * as Types from "./types/public.js";
+export {
+  createImmutableList,
+  createEditableList,
+  createImdb,
+} from "./client/index.js";
+export type {
+  EditableList,
+  Imdb,
+  ImmutableList,
+  PredefinedListKey,
+  PredefinedListRecord,
+  SortOrder,
+  SortType,
+  TitleType,
+} from "./types/public.js";
+export {
+  PREDEFINED_LIST_KEYS,
+  TITLE_TYPES,
+  SORT_TYPES,
+  SORT_ORDER_TYPES,
+} from "./types/public.js";
+
+import {
+  createImmutableList,
+  createEditableList,
+  createImdb,
+} from "./client/index.js";
+import {
+  PREDEFINED_LIST_KEYS,
+  TITLE_TYPES,
+  SORT_TYPES,
+  SORT_ORDER_TYPES,
+} from "./types/public.js";
+
+export default {
+  createImmutableList,
+  createEditableList,
+  createImdb,
+  TITLE_TYPES,
+  SORT_TYPES,
+  SORT_ORDER_TYPES,
+};

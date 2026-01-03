d53c56e0df835ef7f6ba689b9e09ed73b825ad41
younes-alhyan <gameryounes555@gmail.com>
2026-01-03 05:04:06 +0100

    feat(handlers): update getPublicListHandler to fetch list and throw if not found

--- src/handlers/imdb/getPublicListHandler.ts
+++ src/handlers/imdb/getPublicListHandler.ts
@@ -1,9 +1,14 @@
 import type { ImmutableList } from "../../types/public.js";
 import { createImmutableList } from "../../client/index.js";
+import { imdbApi } from "../../utils/imdbApi.js";
 
-export const getPublicListHandler = (
+export const getPublicListHandler = async (
   getCookie: () => string,
   listId: string
-): ImmutableList => {
+): Promise<ImmutableList> => {
+  const response = await imdbApi(getCookie(), `/list/${listId}`);
+
+  if (response.includes("data-styled.g404")) throw new Error("List not found");
+
   return createImmutableList(listId, `/list/${listId}`, {}, getCookie);
 };

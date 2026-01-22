9863ae93c5083cabb4482f8bc5e05d371e076cb3
younes-alhyan <younesalhyan555@gmail.com>
2026-01-22 14:00:00 +0100

    feat(client): implement ImdbClient and list client classes

---
+++ src/client/editableListClient.ts
@@ -0,0 +1,16 @@
+import { ImmutableListClient } from "./immutableListClient.js";
+import type { EditableList } from "../types/public.js";
+import { addListItem, removeListItem } from "../handlers/editableList/index.js";
+
+export class EditableListClient
+  extends ImmutableListClient
+  implements EditableList
+{
+  async addItem(itemId: string): Promise<any> {
+    return await addListItem(this.endpoint, this.id, itemId, this.getCookie);
+  }
+
+  async removeItem(itemId: string): Promise<any> {
+    return await removeListItem(this.endpoint, this.id, itemId, this.getCookie);
+  }
+}

ed439543b406c37f5b8f38f21d22a6193ce8331e
younes-alhyan <younesalhyan555@gmail.com>
2026-01-28 15:00:00 +0100

    refactor(types): update clients methodes to use options object

--- src/client/editableListClient.ts
+++ src/client/editableListClient.ts
@@ -1,22 +1,46 @@
 import { ImmutableListClient } from "./immutableListClient.js";
 import type { EditableList } from "../types/public.js";
-import { addItemHandler, removeItemHandler } from "../handlers/editableList/index.js";
+import {
+  addItemHandler,
+  removeItemHandler,
+} from "../handlers/editableList/index.js";
 
 export class EditableListClient
   extends ImmutableListClient
   implements EditableList
 {
-  async addItem(itemId: string, options?: { rating?: number }): Promise<any> {
-    return addItemHandler(
-      this.endpoint,
-      this.id,
-      itemId,
-      this.getCookie,
-      options
-    );
+  async addItem({
+    titleId,
+    includeListItemMetadata = false,
+    refTagQueryParam = "tt_ov_lst",
+    originalTitleText = false,
+    isInPace = true,
+    rating,
+  }: {
+    titleId: string;
+    includeListItemMetadata?: boolean;
+    refTagQueryParam?: string;
+    originalTitleText?: boolean;
+    isInPace?: boolean;
+    rating?: number;
+  }): Promise<any> {
+    const options = {
+      listId: this.id,
+      titleId,
+      includeListItemMetadata,
+      refTagQueryParam,
+      originalTitleText,
+      isInPace,
+      ...(rating !== undefined ? { rating } : {}),
+    };
+
+    return addItemHandler(this.endpoint, this.getCookie, options);
   }
 
-  async removeItem(itemId: string): Promise<any> {
-    return removeItemHandler(this.endpoint, this.id, itemId, this.getCookie);
+  async removeItem({ titleId }: { titleId: string }): Promise<any> {
+    return removeItemHandler(this.endpoint, this.getCookie, {
+      listId: this.id,
+      titleId,
+    });
   }
 }

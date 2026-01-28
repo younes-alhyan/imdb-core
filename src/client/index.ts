cb7425a2c59184673b8015f00cdd6321273e30dc
younes-alhyan <younesalhyan555@gmail.com>
2026-01-28 17:00:00 +0100

    refactor(client): replace direct client exports with factory functions

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,4 +2,18 @@ import { ImdbClient } from "./imdbClient.js";
 import { ImmutableListClient } from "./immutableListClient.js";
 import { EditableListClient } from "./editableListClient.js";
 
-export { ImdbClient, ImmutableListClient, EditableListClient };
+export const createImmutableList = (
+  id: string,
+  endpoint: string,
+  data: any,
+  getCookie: () => string
+) => new ImmutableListClient(id, endpoint, data, getCookie);
+
+export const createEditableList = (
+  id: string,
+  endpoint: string,
+  data: any,
+  getCookie: () => string
+) => new EditableListClient(id, endpoint, data, getCookie);
+
+export const createImdb = () => new ImdbClient();
